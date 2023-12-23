7ba785fd532049bf3f2fc3b7488d7e0e5896db08
7vTr0x7 <7vtrox7@gmail.com>
2023-12-23 10:53:25 +0530

    third commit

---
+++ Namaste Js/undefined/index.js
@@ -0,0 +1,10 @@
+console.log(a);
+// it  will return undefined bcz when we run it create an execution context and init there are phases
+// 1st phase is memory alloction
+// 2nd phase is code execution
+
+// so in 1st phase all  variable are allocated undefined
+
+var a = 20;
+// in execution phase varibles get assigned with values
+console.log(a);
