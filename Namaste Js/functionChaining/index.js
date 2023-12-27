69e169e9b2de110ed4ee4851fcd764c437b431f5
7vTr0x7 <7vtrox7@gmail.com>
2023-12-27 20:40:15 +0530

    functionChaining

---
+++ Namaste Js/functionChaining/index.js
@@ -0,0 +1,19 @@
+const users = [
+  { firstName: "yash", lastName: "purkar", age: 21 },
+  { firstName: "kimshor", lastName: "tumbe", age: 24 },
+  { firstName: "rumtik", lastName: "chaure", age: 35 },
+  { firstName: "saurabh", lastName: "chaure", age: 31 },
+  { firstName: "rumshi", lastName: "chaure", age: 22 },
+  { firstName: "vtrox", lastName: "chaure", age: 23 },
+];
+
+const under30 = (user) => {
+  return user.age < 30;
+};
+
+const getName = (user) => {
+  return user.firstName;
+};
+
+// chaining map filter reduce
+console.log(users.filter(under30).map(getName));
