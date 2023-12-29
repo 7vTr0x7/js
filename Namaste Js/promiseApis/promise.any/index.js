a823b0e3a64681fe005c19fb6ec733926418c2de
7vTr0x7 <7vtrox7@gmail.com>
2023-12-29 21:53:20 +0530

    promises.any

---
+++ Namaste Js/promiseApis/promise.any/index.js
@@ -0,0 +1,5 @@
+// promise.all takes multiple promises as an array and run all of them simultaneously
+// and will return an array of returned data from promises
+// if any one of the promise fails then then promise.all wil return same error
+// promise.all will get rejected if any promise fails
+// and it will not wait for other promises if any on eof promise fails promise.all will shut down
