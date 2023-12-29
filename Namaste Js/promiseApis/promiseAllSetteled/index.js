0d59f1a80de0266165946bcf7b2420e4f25273c2
7vTr0x7 <7vtrox7@gmail.com>
2023-12-29 21:37:26 +0530

    promises.AllSettle

---
+++ Namaste Js/promiseApis/promiseAllSetteled/index.js
@@ -0,0 +1,4 @@
+// promise.allSettled takes multiple promises as an array and run all of them simultaneously
+// and will return an array of returned data from promises
+// if any one of the promise fails then promise.allSettled will wait for other promises to settle
+// it will return data promises returned in array with error if there is one
