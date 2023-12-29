6fe7befdd7079c564245a5f5906c4b0052831e62
7vTr0x7 <7vtrox7@gmail.com>
2023-12-29 21:55:51 +0530

    promises.race

--- Namaste Js/promiseApis/promiseRace/index.js
+++ Namaste Js/promiseApis/promiseRace/index.js
@@ -2,4 +2,4 @@
 // and will return data from promise
 // only promise to settles first will be returned
 // if the first promise fails then it will return error
-// whatever the case it first promise to resolve of reject it will return it
+// whatever the case it first promise to resolves or rejects it will return it
