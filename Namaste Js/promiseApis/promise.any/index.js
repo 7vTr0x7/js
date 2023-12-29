fd2d18fd3c750009e66a70997a4d09296f048cb6
7vTr0x7 <7vtrox7@gmail.com>
2023-12-29 21:51:46 +0530

    promises.race

--- Namaste Js/promiseApis/promise.any/index.js
+++
@@ -1,5 +0,0 @@
-// promise.race takes multiple promises as an array and run all of them simultaneously
-// and will return data from promise
-// only promise to settles first will be returned
-// if the first promise fails then it will return error
-// whatever the case it first promise to resolve of reject it will return it
