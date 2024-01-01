d7dca415254a7b64010eac6e0028503e3ee54a8d
7vTr0x7 <7vtrox7@gmail.com>
2024-01-01 16:35:05 +0530

    this inside function

--- Namaste Js/this/README.md
+++ Namaste Js/this/README.md
@@ -1,2 +1,16 @@
+note - "this" keyword works differently in strict mode and non-strict mode
+
 1. this in global space
    --> this in global space refers to global object
+
+2. this inside function
+   --> it depends on strict / non-strict mode
+   if its strict mode then this will return undefined
+   and if its non-strict then it will return global object
+
+   --> only in non-strict mode - (this substitution)
+   when the value of this keyword is null or undefined this will be replaced with global object
+
+3. this value depends on how this is called
+   --> x(); - when function is called without reference then its undefined
+   window.x() - when function is called with window reference then its window object
--- Namaste Js/this/index.js
+++ Namaste Js/this/index.js
@@ -1,4 +1,29 @@
-// this in global space
+"use strict";
+
+// ** this in global space
 // this in global scape refers to global object
 
 console.log(this);
+
+// ** this inside function
+
+/*
+  it depends on strict / non-strict mode
+   if its strict mode then this will return undefined
+   and if its non-strict then it will return window object
+*/
+
+function x() {
+  console.log(this);
+}
+
+/*
+   only in non-strict = (this substitution)
+   when the value of this keyword is null or undefined this will be replaced with global object
+*/
+
+// ** this value depends on how this is called
+
+x(); // when function is called without reference then its undefined 
+
+window.x() //hen function is called with window reference then its window object 
